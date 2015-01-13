a95164e77ca4a8f1766b63d8e10b219250bef590
Fenderchamp <treybianchini@gmail.com>
2015-01-13 13:22:02 -0600

    get new jquery stuff try to get this card library to work x-browswer and on the new version of jquery

---
+++ public/javascripts/jquery/new.sh
@@ -0,0 +1,3 @@
+cp jquery-new.js jquery.js
+cp jquery-ui-new.js jquery-ui.js
+cp ../../css/jquery/jquery-ui-new.css ../../css/jquery/jquery-ui.css
---
+++ public/javascripts/jquery/old.sh
@@ -0,0 +1,3 @@
+cp jquery-1.5.2.min.js jquery.js
+cp jquery-ui-1.8.11.custom.min.js jquery-ui.js
+cp ../../css/jquery/jquery-ui-1.8.11.custom.css  ../../css/jquery/jquery-ui.css
